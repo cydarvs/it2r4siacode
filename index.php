903e0960a5f935feb82ace052de2747485f5e363
Cyfred Odarve <cyfredodarve@gmail.com>
2022-03-17 19:20:49 +0800

    This is my first commit

---
+++ index.php
@@ -0,0 +1,9 @@
+This is my program
+
+1. git clone  <repository location>
+2. nano .git/config
+3. Create and Edit a File
+4. git status --> to check what are the updated / new files
+5. git add <filename> --> use to tracked file para ma commit
+   git add -A --> add all files including naka hide 
+6. git commit -m "This is my first commit"
