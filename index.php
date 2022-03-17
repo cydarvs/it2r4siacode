781c1f3da15147498815ef5f568e56f9bcb7a8f7
Cyfred Odarve <cyfredodarve@gmail.com>
2022-03-17 19:39:26 +0800

    Programmer 1 Edited File2 and Index Php

--- index.php
+++ index.php
@@ -7,3 +7,6 @@ This is my program
 5. git add <filename> --> use to tracked file para ma commit
    git add -A --> add all files including naka hide 
 6. git commit -m "This is my first commit"
+7. git push origin master  --> command to submit your code the cloud repo (github)
+
+8. git pull origin master --> get the changes from other users from the remote to your local git repository
